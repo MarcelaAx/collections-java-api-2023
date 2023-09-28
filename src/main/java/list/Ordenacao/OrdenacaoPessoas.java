340aae7cbd75c9212c9cf093c57a47587a16fa6c
Marcela <marcelamachado08@gmail.com>
2023-09-28 16:46:43 -0300

    Exemplos List - Ordenacao e Pesquisa
    Signed-off-by: Marcela <marcelamachado08@gmail.com>

--- src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,2 +1,44 @@
-package main.java.list.Ordenacao;public class OrdenacaoPessoas {
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura= new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        ordenacaoPessoas.adicionarPessoa("Nome: Luis", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome: Julia", 40, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome: Natalia", 31, 1.82);
+        ordenacaoPessoas.adicionarPessoa("Nome: Pedro", 16, 1.56);
+
+        System.out.println(ordenacaoPessoas.pessoaList);
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
 }
